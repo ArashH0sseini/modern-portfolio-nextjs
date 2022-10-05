abd8e5bdfef038eea71b10ab05974372c6878e7d
Arash Hosseini <arashhosseini733@gmail.com>
2022-10-05 16:42:17 +0330

    Building the Skills Section

---
+++ components/Skills.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {};
+
+export default function Skills({}: Props) {
+  return <div className="h-screen flex relative flex-col text-center md:text-left xl:flex-row 
+  max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0  mx-auto items-center">
+    <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">Skills</h3>
+
+    <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">Hover over a skill for currency profieciency</h3>
+  </div>;
+}
