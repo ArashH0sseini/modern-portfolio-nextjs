59be53af88c309af528b377c131cb4b131d06b58
Arash Hosseini <arashhosseini733@gmail.com>
2022-10-06 09:38:15 +0330

    Adding the Home Button and Change Scroll Bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import About from "../components/About";
 import ContactMe from "../components/ContactMe";
 import Header from "../components/Header";
@@ -7,6 +9,7 @@ import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import WorkExperience from "../components/WorkExperience";
+import avatarImage from '../assets/images/avatar3.jpg'
 
 const Home: NextPage = () => {
   return (
@@ -40,6 +43,14 @@ const Home: NextPage = () => {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <Image src={avatarImage} width={40} height={40} className="rounded-full filter grayscale hover:grayscale-0 cursor-pointer" alt="" />
+          </div>
+        </footer>
+      </Link>
     </div>
   );
 };
