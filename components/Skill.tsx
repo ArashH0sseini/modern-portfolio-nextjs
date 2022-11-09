fdaf3b264c67050b79c316cb45fef9eba6304560
Arash Hosseini <arashhosseini733@gmail.com>
2022-11-09 13:18:24 +0330

    Adding Sanity Data in the Skills Section

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import sanityLogo from "../assets/images/sanity.png";
+import { Skill as SkillType } from "../typings";
+import { urlFor } from "../sanity";
 
 type Props = {
   directionLeft?: boolean;
+  skill:SkillType
 };
 
-export default function Skill({ directionLeft }: Props) {
+export default function Skill({ directionLeft,skill }: Props) {
   return (
     <div className="group relative flex cursor-pointer">
-      <motion.div
+      <motion.img
         initial={{
           x: directionLeft ? -200 : 200,
         }}
@@ -19,15 +22,16 @@ export default function Skill({ directionLeft }: Props) {
         className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:-32 xl:h-32 
         filter group-hover:grayscale transition
         duration-300 ease-in-out"
-      >
-        <Image src={sanityLogo} alt="" className="" />
-      </motion.div>
+        src={urlFor(skill.image).url()}
+      />
+        {/* <Image src={urlFor(skill.image).url()} alt="" className="" /> */}
+      {/* </motion.div> */}
       <div
         className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 
       md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0"
       >
         <div className="flex justify-center items-center h-full">
-          <p className="text-3xl font-bold text-black opacity-100">100%</p>
+          <p className="text-3xl font-bold text-black opacity-100">{skill.progress}%</p>
         </div>
       </div>
     </div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import Skill from "./Skill";
 import { motion } from "framer-motion";
+import Skill from "./Skill";
+import { Skill as SkillType } from "../typings";
 
-type Props = {};
+type Props = {
+  skills:SkillType[]
+};
 
-export default function Skills({}: Props) {
+export default function Skills({skills}: Props) {
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -22,20 +25,11 @@ export default function Skills({}: Props) {
       </h3>
 
       <div className="grid grid-cols-4 gap-5">
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+        {
+          skills.map((skill)=>(
+            <Skill key={skill._id} skill={skill} />
+          ))
+        }
       </div>
     </motion.div>
   );
